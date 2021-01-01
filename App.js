87b15e1fab1095a24d6d089c560cdfcaff16e724
AlbrinoFernando <49329356+AlbrinoFernando@users.noreply.github.com>
2021-01-01 09:01:21 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { Header } from "react-native-elements";
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import db from "./localDB";
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
+  Image,
 } from "react-native";
 
 export default class App extends React.Component {
@@ -14,51 +16,117 @@ export default class App extends React.Component {
     super();
     this.state = {
       textTyped: "",
-      displayText: "",
+      isSearchPressed: false,
+      word: "",
+      lexicalCategory: '',
+      examples: [],
+      definition: ""
     };
+  }
+
+  getWord=(word)=>{
+    var searchKeyword = word.toLowerCase();
+    var url = "https://rupinwhitehatjr.github.io/dictionary/"+searchKeyword+".json"
+    console.log(this.state.isSearchPressed)
+    return fetch(url)
+    .then((data)=>{
+      if(data.status===200){
+        return data.json()
+      }else{
+        return null
+      }
+    })
+    .then((response)=>{
+      var responseObject = response
+      if(responseObject){
+        var wordData = responseObject.definitions[0];
+        var definition = wordData.description;
+        var lexicalCategory = wordData.wordtype;
+
+        this.setState({
+          word: this.state.textTyped,
+          definition: definition,
+          lexicalCategory: lexicalCategory
+        })
+      }else{
+        this.setState({
+          word: this.state.textTyped,
+          definition: "Not Found",
+          lexicalCategory: "Not Found"
+        })
+      }
+    })
   }
 
   render() {
     return (
       <View>
         <Header
-          backgroundColor="red"
+          backgroundColor="cyan"
           centerComponent={{
-            text: "Chunky Monkey",
-            style: { fontSize: 30, color: "white" },
+            text: "Dictionary",
+            style: { fontSize: 30, color: "#00666e" },
           }}
+        />
+
+        <Image
+          style={{ width: 175, height: 150, alignSelf: "center", marginTop: 7.5}}
+          source={require("./assets/dictionary.png")}
         />
 
         <TextInput
           style={styles.textInput}
           onChangeText={(text) => {
-            this.setState({ textTyped: text });
+            this.setState({ 
+              textTyped: text,
+              isSearchPressed: this.state.isSearchPressed,
+              word: this.state.word,
+              lexicalCategory: this.state.lexicalCategory,
+              examples: [],
+              definition: this.state.definition
+            });
           }}
           value={this.state.textTyped}
         />
 
         <TouchableOpacity
           style={styles.button}
-          onPress={() => {
-            this.setState({ displayText: this.state.textTyped });
-          }}
-        >
-          <Text style={styles.buttonText}>Create Text Chunks!</Text>
+          onPress={
+            ()=>{
+              this.setState({
+                isSearchPressed: true
+              })
+              this.getWord(this.state.textTyped);
+            }
+          }>
+          <Text style={styles.buttonText}>Search!</Text>
         </TouchableOpacity>
+        
+        {this.state.isSearchPressed &&
+          <Text style={styles.infoTitle}>
+            Word: {" "}
+            <Text style={styles.info}>
+              {this.state.word}
+            </Text>
+            {"\n"}
+            Type: {" "}
+            <Text style={styles.info}>
+              {this.state.lexicalCategory}
+            </Text>
+            {"\n"}
+            Definition: {"\n"}
+            <Text style={styles.info}>
+              {this.state.definition}
+            </Text>
+          </Text>
+        }
 
-        <Text>{this.state.displayText}</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   textInput: {
     borderColor: "#4a565c",
     borderWidth: 2,
@@ -81,4 +149,20 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 20,
   },
+  infoContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  infoTitle:{
+    fontWeight: "bold",
+    color: "#007de3",
+    fontSize: 20,
+    padding: 10
+  },
+  info:{
+    fontWeight: "normal",
+    color: "black"
+  }
 });
